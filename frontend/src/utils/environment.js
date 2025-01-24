cf798c072854ee5cbac3d4e2da6d6bedf7564ecb
Rohandeep Patra <rohandp100@gmail.com>
2025-01-24 22:32:38 +0530

    home , create meeting room

--- frontend/src/utils/environment.js
+++
@@ -1,6 +0,0 @@
-const IS_PROD = process.env.NODE_ENV === "production"; // Automatically detects production environment
-const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com"
-  : "http://localhost:8080";
-
-export default server;
