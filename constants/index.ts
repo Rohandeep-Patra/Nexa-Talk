d1cd184c84709b835dcdee2956d3ec6c33287f03
Rohandeep Patra <rohandp100@gmail.com>
2025-01-26 15:41:07 +0530

    fix bug stage 1

--- constants/index.ts
+++ constants/index.ts
@@ -1,30 +1,30 @@
 export const sidebarLinks = [
-    {
-        label: 'Home',
-        route: '/',
-        imgUrl: '/icons/home.svg',
-    },
-    {
-        label: 'Upcoming Meetings',
-        route: '/upcoming',
-        imgUrl: '/icons/upcoming.svg',
-    },
-    {
-        label: 'Previous Meetings',
-        route: '/previous',
-        imgUrl: '/icons/previous.svg',
-    },  
-    {
-        label: 'Recordings',
-        route: '/recordings',
-        imgUrl: '/icons/video.svg',
-    },
-    {
-        label: 'Personal Room',
-        route: '/personal-room',
-        imgUrl: '/icons/add-personal.svg',
-    },
-]
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/Home.svg",
+  },
+  {
+    label: "Upcoming Meetings",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous Meetings",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/recordings.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
 
 
 export const avatarImages = [
